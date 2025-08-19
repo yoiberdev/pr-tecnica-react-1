7f7f33c7ad6467b9768ebca2f0e9379cc9bfb991
Yoiber dev <yoiber696@gmail.com>
2025-08-19 18:13:27 -0500

    Ajusta la posición del componente TextWrapper para mejorar la alineación visual

--- src/components/TextWrapper.tsx
+++ src/components/TextWrapper.tsx
@@ -10,7 +10,7 @@ const TextWrapper = ({
   description = "The mountains are calling" 
 }: TextWrapperProps) => {
   return (
-    <div className="text-white flex flex-col font-bold absolute top-1/2 left-90 hover:left-100  transform -translate-y-1/2 z-20">
+    <div className="text-white flex flex-col font-bold absolute top-1/2 left-100  transform -translate-y-1/2 z-20">
       <div className="data-title text-4xl mb-2 transition-all duration-300 ease-in-out">
         <span className="drop-shadow-lg">{title}</span>
       </div>
